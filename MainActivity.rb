56a2de9eacbcac2e096fc862624696f6f9a9eebf
Seacken <kseniya.suslenkova@gmail.com>
2022-10-10 12:31:34 +0300

    Creating main structure

---
+++ MainActivity.rb
@@ -0,0 +1,28 @@
+print "Choose the action:\n
+1. Puts user's information
+2. Puts avatars
+3. Puts pictures libraries\n
+Print 0 to exit\n"
+
+#dbconnect = DBConnect.new('root', 'ThisIsMyPassword!1', 'Users_Pictures')
+#dbconnect.Connect
+esc = false;
+while !esc
+  action = gets.chomp;
+  case action
+  when "0"
+    puts "Exit?
+        Y-yes N-no\n"
+    answer = gets.chomp.upcase
+    if answer=="Y" then esc=true
+    end
+  when "1"
+    #dbconnect.GetUsersInfo
+  when "2"
+    #dbconnect.GetIconInfo
+  when "3"
+    #dbconnect.GetPicturesInfo
+  else
+    puts "Wrong input, try again, please.\n"
+  end
+end
